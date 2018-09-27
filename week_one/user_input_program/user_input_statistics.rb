584858ce306a8ac8f0521bca2c0a9e628c9b438c
Erika Keith <erikakeith13@gmail.com>
2018-09-27 02:58:06 +0000

    changed stats

--- week_one/user_input_program/user_input_statistics.rb
+++ week_one/user_input_program/user_input_statistics.rb
@@ -1,10 +1,18 @@
+loop do
+size = [5,6,7,8,9,10]
+color = ["red", "blue", "green", "black"]
+shoes =["Yes", "No"]
+puts "Enter your shoe size:"
+size = gets
+size << size
+puts "What color shoe do you need?"
+color = gets
+color << color
+puts "Did you find the right shoes?"
+shoes = gets.chomp
+if shoes == "Yes" 
+  break
+end
 
-['Hello', 'Goodbye']
- 
- sharks = ["Hammerhead", "Great White", "Tiger"]
-  loop do
-      puts "How many great white sharks are there?"
-  end
 
-
-
+end
